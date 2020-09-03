1930c2e43aeddea7d53d8faed98d49c8f57802dd
cslplasma <54950381+cslplasma@users.noreply.github.com>
2020-09-03 18:44:01 -0400

    Item6: Avoid creating unnecessary objects

--- src/main/java/effectivejava/chapter2/item6/RomanNumerals.java
+++ src/main/java/effectivejava/chapter2/item6/RomanNumerals.java
@@ -1,40 +1,51 @@
 package effectivejava.chapter2.item6;
 import java.util.regex.Pattern;
 
+/**
+ * 
+ * - Item 6: Avoid creating unnecessary objects
+ * 
+ * String s = new String("bikini"); // DON'T DO THIS!
+ * String s = "bikini"; //Improved version
+ * This version uses a single String instance, rather than creating a new one each time it is executed.
+ * Furthermore, it is guaranteed that the object will be reused by any other code running in the same virtual machine 
+ * that happens to contain the same string literal
+ *
+ */
 // Reusing expensive object for improved performance (Pages 22 and 23)
 public class RomanNumerals {
-    // Performance can be greatly improved! (Page 22)
-    static boolean isRomanNumeralSlow(String s) {
-        return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
-                + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
-    }
+	// Performance can be greatly improved! (Page 22)
+	
+	// While String.matches is the easiest way to check if a string matches a
+	// regular expression, it�s not suitable for repeated use in performance-critical situations.
+	static boolean isRomanNumeralSlow(String s) {
+		return s.matches("^(?=.)M*(C[MD]|D?C{0,3})" + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+	}
 
-    // Reusing expensive object for improved performance (Page 23)
-    private static final Pattern ROMAN = Pattern.compile(
-            "^(?=.)M*(C[MD]|D?C{0,3})"
-                    + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+	// Reusing expensive object for improved performance (Page 23)
+	private static final Pattern ROMAN = Pattern
+			.compile("^(?=.)M*(C[MD]|D?C{0,3})" + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
 
-    static boolean isRomanNumeralFast(String s) {
-        return ROMAN.matcher(s).matches();
-    }
+	static boolean isRomanNumeralFast(String s) {
+		return ROMAN.matcher(s).matches();
+	}
 
-    public static void main(String[] args) {
-        int numSets = Integer.parseInt(args[0]);
-        int numReps = Integer.parseInt(args[1]);
-        boolean b = false;
+	public static void main(String[] args) {
+		int numSets = Integer.parseInt(args[0]);
+		int numReps = Integer.parseInt(args[1]);
+		boolean b = false;
 
-        for (int i = 0; i < numSets; i++) {
-            long start = System.nanoTime();
-            for (int j = 0; j < numReps; j++) {
-                b ^= isRomanNumeralSlow("MCMLXXVI");  // Change Slow to Fast to see performance difference
-            }
-            long end = System.nanoTime();
-            System.out.println(((end - start) / (1_000. * numReps)) + " μs.");
-        }
+		for (int i = 0; i < numSets; i++) {
+			long start = System.nanoTime();
+			for (int j = 0; j < numReps; j++) {
+				b ^= isRomanNumeralSlow("MCMLXXVI"); // Change Slow to Fast to see performance difference
+			}
+			long end = System.nanoTime();
+			System.out.println(((end - start) / (1_000. * numReps)) + " μs.");
+		}
 
-        // Prevents VM from optimizing away everything.
-        if (!b)
-            System.out.println();
-    }
+		// Prevents VM from optimizing away everything.
+		if (!b)
+			System.out.println();
+	}
 }
-
--- src/main/java/effectivejava/chapter2/item6/Sum.java
+++ src/main/java/effectivejava/chapter2/item6/Sum.java
@@ -1,29 +1,38 @@
 package effectivejava.chapter2.item6;
 
-import java.util.Comparator;
-
 // Hideously slow program! Can you spot the object creation? (Page 24)
+// “Don’t create a new object when you should reuse an existing one,” 
+// while Item 50 says, “Don’t reuse an existing object when you should create a new one.”
+
 public class Sum {
-    private static long sum() {
-        Long sum = 0L;
-        for (long i = 0; i <= Integer.MAX_VALUE; i++)
-            sum += i;
-        return sum;
-    }
+	
+	private static long sum() {
+		// The variable sum is declared as a Long instead of a long, which means that
+		// the program constructs about 2^31 unnecessary Long instances (roughly one 
+		// for each time the long i is added to the Long  sum).
 
-    public static void main(String[] args) {
-        int numSets = Integer.parseInt(args[0]);
-        long x = 0;
+		// Changing the declaration of sum from Long to long reduces the runtime from
+		// 6.3 seconds to 0.59 seconds on my machine. The lesson is clear: prefer primitives
+		// to  boxed primitives, and watch out for unintentional autoboxing.
+		Long sum = 0L;
+		for (long i = 0; i <= Integer.MAX_VALUE; i++)
+			sum += i;
+		return sum;
+	}
 
-        for (int i = 0; i < numSets; i++) {
-            long start = System.nanoTime();
-            x += sum();
-            long end = System.nanoTime();
-            System.out.println((end - start) / 1_000_000. + " ms.");
-        }
+	public static void main(String[] args) {
+		int numSets = Integer.parseInt(args[0]);
+		long x = 0;
 
-        // Prevents VM from optimizing away everything.
-        if (x == 42)
-            System.out.println();
-    }
+		for (int i = 0; i < numSets; i++) {
+			long start = System.nanoTime();
+			x += sum();
+			long end = System.nanoTime();
+			System.out.println((end - start) / 1_000_000. + " ms.");
+		}
+
+		// Prevents VM from optimizing away everything.
+		if (x == 42)
+			System.out.println();
+	}
 }
